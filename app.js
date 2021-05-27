8c6f08f9ba623d9007b6dc7867e6d0d562b9fc4e
Manish Kumar <manish@oc7855550034.ibm.com>
2021-05-27 10:05:25 +0530

    removed message manual review comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! we are creating something on AWS using DevOps, Added manual review stage'));
+app.get('/', (req, res) => res.send('Hello World! we are creating something on AWS using DevOps, removed manual review comment'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
